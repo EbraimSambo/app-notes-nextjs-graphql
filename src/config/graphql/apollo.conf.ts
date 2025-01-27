8d54da2bbbfe46c5fbf8f8f7620d80c64ba807c7
ebraimsambo <ebraimsambo@gmail.com>
2025-01-27 09:21:54 +0100

    down

--- src/config/graphql/apollo.conf.ts
+++ src/config/graphql/apollo.conf.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:3002/graphql',
+    uri: process.env.PUBLIC_GRAPHQL_URL,
     cache: new InMemoryCache(),
 });
